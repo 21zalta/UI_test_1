95bad7437c4eb1dc820e985a7f10e14cedd16fcd
HOME-PC\User <horsea16@gmail.com>
2026-09-16 12:05:59 +0300

    Обновила логику параметризованного теста
    Signed-off-by: HOME-PC\User <horsea16@gmail.com>

--- test_shop.py
+++ test_shop.py
@@ -1,10 +1,24 @@
+import pytest
 from playwright.sync_api import Page, expect
 
-def test_complete_purchase_flow(page: Page):
+login_test_date = [
+    ("standard_user", "secret_sauce", "success"),
+    ("locked_out_user", "secret_sauce", "error"),
+    ("problem_user","secret_sauce", "success"),
+    ("Oleg", "dff", "dff"),
+]
+
+@pytest.mark.parametrize("username, password, expected_result", login_test_date)
+def test_saucedemo_login_parametrized(page: Page, username: str, password: str, expected_result: str):
     page.goto("https://www.saucedemo.com/")
-    page.locator("#user-name").fill("standard_user")
-    page.locator("#password").fill("secret_sauce")
+    page.locator("#user-name").fill(username)
+    page.locator("#password").fill(password)
     page.locator("#login-button").click()
+
+    if expected_result == "success":
+        expect(page.locator(".inventory_list")).to_be_visible()
+    elif expected_result == "error":
+        expect(page.locator("[data-test='error']")).to_be_visible()
     expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
 
     page.locator("#add-to-cart-sauce-labs-backpack").click()
