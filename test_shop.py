a21a66a93c797b2ec2b941212e9f8ee9f1464fa3
HOME-PC\User <horsea16@gmail.com>
2026-09-17 11:36:48 +0300

    Обновила логику теста
    Signed-off-by: HOME-PC\User <horsea16@gmail.com>

--- test_shop.py
+++ test_shop.py
@@ -5,7 +5,7 @@ login_test_date = [
     ("standard_user", "secret_sauce", "success"),
     ("locked_out_user", "secret_sauce", "error"),
     ("problem_user","secret_sauce", "success"),
-    ("Oleg", "dff", "dff"),
+    ("Oleg", "dff", "error"),
 ]
 
 @pytest.mark.parametrize("username, password, expected_result", login_test_date)
@@ -16,22 +16,24 @@ def test_saucedemo_login_parametrized(page: Page, username: str, password: str, 
     page.locator("#login-button").click()
 
     if expected_result == "success":
+        expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
         expect(page.locator(".inventory_list")).to_be_visible()
+        page.locator("#add-to-cart-sauce-labs-backpack").click()
+        expect(page.locator(".shopping_cart_badge")).to_have_text("1")
+
+        page.locator(".shopping_cart_link").click()
+        expect(page.locator(".title")).to_have_text("Your Cart")
+
+        page.locator("#checkout").click()
+
+        page.locator("#first-name").fill("Alex")
+        page.locator("#last-name").fill("Cooper")
+        page.locator("#postal-code").fill("2345")
+        page.locator("#continue").click()
+        page.locator("#finish").click()
+        expect(page.locator(".complete-header")).to_have_text("Thank you for your order!")
     elif expected_result == "error":
         expect(page.locator("[data-test='error']")).to_be_visible()
-    expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
+        expect(page).to_have_url("https://www.saucedemo.com/")
 
-    page.locator("#add-to-cart-sauce-labs-backpack").click()
-    expect(page.locator(".shopping_cart_badge")).to_have_text("1")
 
-    page.locator(".shopping_cart_link").click()
-    expect(page.locator(".title")).to_have_text("Your Cart")
-
-    page.locator("#checkout").click()
-
-    page.locator("#first-name").fill("Alex")
-    page.locator("#last-name").fill("Cooper")
-    page.locator("#postal-code").fill("2345")
-    page.locator("#continue").click()
-    page.locator("#finish").click()
-    expect(page.locator(".complete-header")).to_have_text("Thank you for your order!")
